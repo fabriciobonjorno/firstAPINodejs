f748989e499fb6d36ea86b06dcc7e0f6193a02a4
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-06-03 15:01:20 -0400

    First API with NodeJs]

---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Fabricio'
+  },
+  {
+    id: 2,
+    name: 'Fabricio'
+  },
+  {
+    id: 3,
+    name: 'Fabricio'
+  },
+  {
+    id: 4,
+    name: 'Fabricio'
+  },
+  {
+    id: 5,
+    name: 'Fabricio'
+  }
+];
